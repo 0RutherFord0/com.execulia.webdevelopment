08cf0ffd57134fe05fa315a47ad8db5b909e3675
Aditya Tawade <adityatwd0@gmail.com>
2021-05-08 01:50:55 +0530

    Added Connect Wifi Toggle

--- app/src/main/java/com/execulia/webdevelopment/Splash_Screen.java
+++ app/src/main/java/com/execulia/webdevelopment/Splash_Screen.java
@@ -23,7 +23,7 @@ public class Splash_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         //Animation
